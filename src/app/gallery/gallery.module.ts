66b403bc3daf3f0edfae304ae5411973ffb33a05
Sam M <emberdesignltd@protonmail.com>
2021-06-21 14:58:35 +0100

    feat(Gallery): Implement lazyLoading directive

--- src/app/gallery/gallery.module.ts
+++ src/app/gallery/gallery.module.ts
@@ -1,14 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { LazyLoadImagesModule } from '../directives/lazy-load-images/lazy-load-images.module';
 import { GalleryRoutingModule } from './gallery-routing.module';
 import { GalleryComponent } from './gallery/gallery.component';
 import { ImageComponent } from './image/image.component';
 
 @NgModule({
-  declarations: [
-    GalleryComponent,
-    ImageComponent
-  ],
-  imports: [CommonModule, GalleryRoutingModule],
+  declarations: [GalleryComponent, ImageComponent],
+  imports: [CommonModule, GalleryRoutingModule, LazyLoadImagesModule],
 })
 export class GalleryModule {}
